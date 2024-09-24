cf3037e38b65c064f36b9b5ee0d9621dac529113
insines <insinesbl@ya.ru>
2024-09-24 20:57:11 +0300

    upd

--- src/js/blog/update-article-content.js
+++ src/js/blog/update-article-content.js
@@ -1,11 +1,16 @@
 import { createArticleQuote } from './create-article-quote.js'
 
 export const updateArticleContent = () => {
-  document.querySelectorAll('[data-article="content"] p').forEach(paragraph => {
-    const text = paragraph.textContent
+  const pathname = window.location.pathname
 
-    if (text.includes('<<Quote')) {
-      createArticleQuote(paragraph)
-    }
-  })
+  if (pathname.includes('/blog/') || pathname.includes('/customer-projects/'))
+    document
+      .querySelectorAll('[data-article="content"] p')
+      .forEach(paragraph => {
+        const text = paragraph.textContent
+
+        if (text.includes('<<Quote')) {
+          createArticleQuote(paragraph)
+        }
+      })
 }
