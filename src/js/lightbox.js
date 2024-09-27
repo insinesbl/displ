cf9fe0fe7764f02afcec167f1be2d87f74682456
insines <insinesbl@ya.ru>
2024-09-27 17:18:42 +0300

    upd cases

---
+++ src/js/lightbox.js
@@ -0,0 +1,26 @@
+const lightboxWrapper = document.querySelector('[data-lightbox="wrap"]')
+
+const closeLightbox = () => {
+  lightboxWrapper.classList.add('hide')
+  document.removeEventListener('keydown', onLightboxEscKeydown)
+}
+
+const onLightboxEscKeydown = evt => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.preventDefault()
+    closeLightbox()
+  }
+}
+
+export const openLightbox = imageSrc => {
+  const lightboxImage = lightboxWrapper.querySelector('[data-lightbox="image"]')
+
+  lightboxImage.src = imageSrc
+  lightboxWrapper.classList.remove('hide')
+  document.addEventListener('keydown', onLightboxEscKeydown)
+
+  const lightboxCloseButton = lightboxWrapper.querySelector(
+    '[data-lightbox="close"]'
+  )
+  lightboxCloseButton.addEventListener('click', () => closeLightbox())
+}
--- src/js/recources/update-cases-content.js
+++ src/js/recources/update-cases-content.js
@@ -1,3 +1,4 @@
+import { openLightbox } from '../lightbox.js'
 import { createArticleQuote } from './create-article-quote.js'
 
 export const updateCasesContent = () => {
@@ -13,11 +14,12 @@ export const updateCasesContent = () => {
     // Add icons to titles
     const headerClasses = {
       Goals: 'goals',
+      Goal: 'goals',
       'Work process': 'work-process',
+      'Work proces': 'work-process',
+      Results: 'results',
       Results: 'results',
     }
-
-    // Находим все элементы h2
     document
       .querySelector('[data-article="content"]')
       .querySelectorAll('h2')
@@ -30,5 +32,84 @@ export const updateCasesContent = () => {
           )
         }
       })
+
+    // hover images
+    const gridImages = document.querySelectorAll(
+      '[data-article-case="grid-media-img"]'
+    )
+    const largeImage = document.querySelector(
+      '[data-article-case="media-img-large"]'
+    )
+
+    largeImage.addEventListener('click', event => {
+      event.preventDefault()
+      openLightbox(event.currentTarget.getAttribute('src'))
+    })
+
+    const gridVideo = document.querySelector(
+      '[data-article-case="grid-media-video"]'
+    )
+    const largeVideo = document.querySelector(
+      '[data-article-case="media-video-large"]'
+    )
+
+    // Логика для изображений
+    if (gridImages.length > 0 && largeImage) {
+      gridImages.forEach(item => {
+        item.addEventListener('mouseover', () => {
+          const src = item.querySelector('img').getAttribute('src')
+          largeImage.setAttribute('src', src)
+
+          largeImage.classList.remove('hide')
+          largeVideo.classList.add('hide')
+        })
+
+        if (window.innerWidth < 768) {
+          item.querySelector('img').addEventListener('click', event => {
+            event.preventDefault()
+            console.log(1)
+            openLightbox(event.currentTarget.getAttribute('src'))
+          })
+        }
+      })
+    }
+
+    if (gridVideo && largeVideo) {
+      gridVideo.addEventListener('mouseover', () => {
+        largeImage.classList.add('hide')
+        largeVideo.classList.remove('hide')
+      })
+    }
+
+    if (gridImages.length > 0) {
+      const firstImage = gridImages[0]
+      const mouseoutEvent = new Event('mouseover', {
+        bubbles: true,
+        cancelable: true,
+      })
+      firstImage.dispatchEvent(mouseoutEvent)
+    }
+
+    // move video to images grid
+    const listImages = document.querySelector('[fs-cmsstatic-element="list"]')
+    const videoItem = document.querySelector(
+      '[data-article-case="grid-media-video"]'
+    )
+    if (videoItem) {
+      if (videoItem.children.length > 0) {
+        videoItem.classList.remove('hide')
+        listImages.insertAdjacentElement('afterbegin', videoItem)
+      } else {
+        videoItem.remove()
+      }
+    }
   }
 }
+
+// const lightboxWrap = document.querySelector(
+//   '[data-article="lightbox-wrap"]'
+// )
+// const lightbox = document.querySelector('[data-article="lightbox"]')
+// if (lightboxWrap && lightbox) {
+//   lightboxWrap.setAttribute('src', src)
+//   lightbox.setAttribute('src', src)
