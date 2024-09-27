4e20c9a55c5f22fbf0a34d8c7afd4e1dc4b2adb6
insines <insinesbl@ya.ru>
2024-09-27 17:18:52 +0300

    upd tag

--- src/js/load-main.js
+++ src/js/load-main.js
@@ -3,7 +3,7 @@ const script = document.createElement('script')
 const host =
   localDev === '1'
     ? 'http://localhost:5500'
-    : 'https://cdn.jsdelivr.net/gh/insinesbl/displ@v1.0.21'
+    : 'https://cdn.jsdelivr.net/gh/insinesbl/displ@v1.0.22'
 script.type = 'module'
 script.async = true
 script.src = `${host}/src/js/index.js`
