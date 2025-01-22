aec2c29f8f5c875309a9d61318ebca8fdbd9e3d2
insines <insinesbl@ya.ru>
2025-01-22 13:22:57 +0300

    fix

--- src/js/update-local-dropdown.js
+++ src/js/update-local-dropdown.js
@@ -2,4 +2,5 @@ export const updateLocalDropdown = () => {
   const currentLocal = $('html').attr('data-wf-locale')
   const currentLocalName = $(`[data-local-item="${currentLocal}"]`).text()
   $('[data-local-toggle]').text(currentLocalName)
+  $(`[data-local-item="${currentLocal}"]`).text() // remove current local in dropdown list
 }
