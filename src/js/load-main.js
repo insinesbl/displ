aed4a9bc101d56374b18508b6ad56bc33f731a7f
insines <insinesbl@ya.ru>
2025-03-05 13:13:25 +0300

    fix

--- src/js/load-main.js
+++ src/js/load-main.js
@@ -3,7 +3,7 @@ const script = document.createElement('script')
 const host =
   localDev === '1'
     ? 'http://localhost:5500'
-    : 'https://cdn.jsdelivr.net/gh/insinesbl/displ@v1.0.42'
+    : 'https://cdn.jsdelivr.net/gh/insinesbl/displ@v1.0.43'
 script.type = 'module'
 script.async = true
 script.src = `${host}/src/js/index.js`
